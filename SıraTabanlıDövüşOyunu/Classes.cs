282b2a28eeac9103ccfa543128718e0e0c50d6df
Nevzat <Nevzat@NEVZATAKYUZ>
2025-07-08 01:33:43 +0300

    Proje dosyası ekle.

---
+++ SıraTabanlıDövüşOyunu/Classes.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection.Metadata.Ecma335;   
+
+namespace BaseClasses;
+
+public class Entity
+
+{
+    public int Health { get; set; } = 0;
+    public int AttackPower { get; set; } = 0;
+    public int DefensePower { get; set; } = 0;
+
+    public string Name { get; set; }
+
+    public Entity(int health, int attackPower, int defensePower, string name)
+    {
+        Name = name;
+        Health = health;
+        AttackPower = attackPower;
+        DefensePower = defensePower;
+    }
+
+    public bool IsAlive()
+    {
+        return Health > 0;
+    }
+    public int TakeDamage()
+    {
+        return DefensePower -= AttackPower;
+    }
+}
+ 
